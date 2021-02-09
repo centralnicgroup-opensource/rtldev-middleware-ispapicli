53f869c4924d2d51706971464c514ea5cc50f38a
Rashad Jamara <rjamara@hexonet.net>
2021-02-09 16:24:10 +0100

    build(minor changes): fixing a few code errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,33 @@
-$(document).ready(function () {
-	/* ===== Stickyfill ===== */
-	/* Ref: https://github.com/wilddeer/stickyfill */
-	// Add browser support to position: sticky
-	const elements = $('.sticky');
-	Stickyfill.add(elements);
+$(document).ready(() => {
+    /* ===== Stickyfill ===== */
+    /* Ref: https://github.com/wilddeer/stickyfill */
+    // Add browser support to position: sticky
+    const elements = $('.sticky')
+    Stickyfill.add(elements)
 
-	/* Activate scrollspy menu */
-	$('body').scrollspy({ target: '#doc-menu', offset: 100 });
+    /* Activate scrollspy menu */
+    $('body').scrollspy({
+        target: '#doc-menu',
+        offset: 100
+    })
 
-	/* Smooth scrolling */
-	$('a.scrollto').on('click', function (e) {
-		// store hash
-		const target = this.hash;
-		e.preventDefault();
-		$('body').scrollTo(target, 800, { offset: 0, axis: 'y' });
-	});
+    /* Smooth scrolling */
+    $('a.scrollto').on('click', function (event) {
+        const target = this.hash
+        event.preventDefault()
+        $('body').scrollTo(target, 800, {
+            offset: 0,
+            axis: 'y'
+        })
+    })
 
-	/* Bootstrap lightbox */
-	/* Ref: http://ashleydw.github.io/lightbox/ */
+    /* Bootstrap lightbox */
+    /* Ref: http://ashleydw.github.io/lightbox/ */
 
-	$(document).delegate('*[data-toggle="lightbox"]', 'click', function (e) {
-		e.preventDefault();
-		$(this).ekkoLightbox();
-	});
-});
+    $(document).delegate('*[data-toggle="lightbox"]', 'click', function (
+        event
+    ) {
+        event.preventDefault()
+        $(this).ekkoLightbox()
+    })
+})
--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,44 @@
-const { src, parallel } = require('gulp');
-const gulp = require('gulp');
-const xo = require('gulp-xo');
-const csslint = require('gulp-csslint');
-const htmlhint = require("gulp-htmlhint");
+const { src, parallel } = require('gulp')
+const gulp = require('gulp')
+const xo = require('gulp-xo')
+const csslint = require('gulp-csslint')
+const htmlhint = require('gulp-htmlhint')
 // import { htmlValidator } from 'gulp-w3c-html-validator';
-
 
 /*
 validate css code
 */
 function css() {
-  return gulp.src('assets/css/*.css')
-    .pipe(csslint())
-    .pipe(csslint.formatter()); // display errors
-  //.pipe(csslint.formatter('fail')); // fail on error 
+    return gulp
+        .src('assets/css/*.css')
+        .pipe(csslint('csslintrc.json'))
+        .pipe(csslint.formatter()) // display errors
+        .pipe(csslint.formatter('fail')) // fail on error
 }
 
 /*
 validate html
 */
 function html() {
-  return gulp.src("*.html")
-    .pipe(htmlhint()) // check errors
-    .pipe(htmlhint.reporter()); //display errors
-  //.pipe(htmlhint.failAfterError()); // fail on error
+    return gulp
+        .src('*.html')
+        .pipe(htmlhint()) // check errors
+        .pipe(htmlhint.reporter()) //display errors
+        .pipe(htmlhint.failAfterError()) // fail on error
 }
 
 /*
 validate javascript with eslint, ignores assetes/plugins
 */
 function eslintXO() {
-  return src(['assets/js/**/*.js'])
-    .pipe(xo({
-      "globals": [
-        "jQuery",
-        "$",
-        "document"
-      ]
-    }))
-    .pipe(xo.format()); // display errors
-  //.pipe(xo.failAfterError()); // fail on errors
+    return src(['assets/js/**/*.js'])
+        .pipe(
+            xo({
+                globals: ['jQuery', '$', 'document', 'Stickyfill'] // Stickyfill is a js library
+            })
+        )
+        .pipe(xo.format()) // display errors
+        .pipe(xo.failAfterError()) // fail on errors
 }
 
-exports.validate = parallel(css, html, eslintXO);
+exports.validate = parallel(css, html, eslintXO)
