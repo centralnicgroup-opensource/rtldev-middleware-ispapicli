4d550021d516c3cf32f395a4c63c8dfcb7eff8de
Rashad Jamara <rjamara@hexonet.net>
2021-02-11 13:57:04 +0100

    build(major validation): major changes to validation of style and code quality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,31 @@
 $(document).ready(() => {
-	/* ===== Stickyfill ===== */
-	/* Ref: https://github.com/wilddeer/stickyfill */
-	// Add browser support to position: sticky
-	const elements = $('.sticky')
-	Stickyfill.add(elements)
+  /* ===== Stickyfill ===== */
+  /* Ref: https://github.com/wilddeer/stickyfill */
+  // Add browser support to position: sticky
+  const elements = $(".sticky");
+  Stickyfill.add(elements);
 
-	/* Activate scrollspy menu */
-	$('body').scrollspy({
-		target: '#doc-menu',
-		offset: 100
-	})
+  /* Activate scrollspy menu */
+  $("body").scrollspy({
+    target: "#doc-menu",
+    offset: 100,
+  });
 
-	/* Smooth scrolling */
-	$('a.scrollto').on('click', function (event) {
-		const target = this.hash
-		event.preventDefault()
-		$('body').scrollTo(target, 800, {
-			offset: 0,
-			axis: 'y'
-		})
-	})
+  /* Smooth scrolling */
+  $("a.scrollto").on("click", function (event) {
+    const target = this.hash;
+    event.preventDefault();
+    $("body").scrollTo(target, 800, {
+      offset: 0,
+      axis: "y",
+    });
+  });
 
-	/* Bootstrap lightbox */
-	/* Ref: http://ashleydw.github.io/lightbox/ */
+  /* Bootstrap lightbox */
+  /* Ref: http://ashleydw.github.io/lightbox/ */
 
-	$(document).delegate('*[data-toggle="lightbox"]', 'click', function (
-		event
-	) {
-		event.preventDefault()
-		$(this).ekkoLightbox()
-	})
-})
+  $(document).delegate('*[data-toggle="lightbox"]', "click", function (event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+  });
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,71 @@
-const { src, parallel } = require('gulp')
-const gulp = require('gulp')
-const xo = require('gulp-xo')
-const csslint = require('gulp-csslint')
-const htmlhint = require('gulp-htmlhint')
-// import { htmlValidator } from 'gulp-w3c-html-validator';
+const { src, dest, series } = require("gulp");
+const xo = require("gulp-xo");
+const stylelint = require("gulp-stylelint");
+const htmlhint = require("gulp-htmlhint");
+const prettier = require("gulp-prettier");
 
 /*
 validate css code
 */
 function css() {
-	return gulp
-		.src('assets/css/*.css')
-		.pipe(csslint('csslintrc.json'))
-		.pipe(csslint.formatter()) // display errors
-	//.pipe(csslint.formatter('fail')) // fail on error
+  return src("assets/css/*.css").pipe(
+    stylelint({
+      failAfterError: true,
+      reportOutputDir: "reports/lint",
+      reporters: [{ formatter: "verbose", console: true }],
+      debug: true,
+    })
+  );
 }
 
 /*
 validate html
 */
 function html() {
-	return gulp
-		.src('*.html')
-		.pipe(htmlhint()) // check errors
-		.pipe(htmlhint.reporter()) //display errors
-		.pipe(htmlhint.failAfterError()) // fail on error
+  return src("*.html")
+    .pipe(htmlhint()) // check errors
+    .pipe(htmlhint.reporter()) //display errors
+    .pipe(htmlhint.failAfterError()); // fail on error
 }
 
 /*
 validate javascript with eslint, ignores assetes/plugins
 */
 function eslintXO() {
-	return src(['assets/js/**/*.js'])
-		.pipe(
-			xo({
-				globals: ['jQuery', '$', 'document', 'Stickyfill'] // Stickyfill is a js library
-			})
-		)
-		.pipe(xo.format()) // display errors
-		.pipe(xo.failAfterError()) // fail on errors
+  return src(["assets/js/**/*.js"])
+    .pipe(
+      xo({
+        globals: ["jQuery", "$", "document", "window", "Stickyfill"], // Stickyfill is a js library
+      })
+    )
+    .pipe(xo.format()) // display errors
+    .pipe(xo.failAfterError()); // fail on errors
 }
 
-exports.validate = parallel(css, html, eslintXO)
+/*
+Check our code based on prettier formatter
+*/
+function prettierCheck() {
+  return src([
+    "assets/js/*.js",
+    "assets/css/*.css",
+    ".prettierrc.js",
+    "gulpfile.js",
+    "*.html",
+  ]).pipe(prettier.check());
+}
+
+// can be triggered locally to format your code
+exports.prettier = function () {
+  return src([
+    "assets/js/*.js",
+    "assets/css/*.css",
+    ".prettierrc.js",
+    "gulpfile.js",
+    "*.html",
+  ])
+    .pipe(prettier())
+    .pipe(dest((file) => file.base));
+};
+// automatically triggered upon each push or pull request
+exports.validate = series(prettierCheck, eslintXO, css, html);
