bf6b47801897a273dc026433dffe491f87a18279
Rashad Jamara <rjamara@hexonet.net>
2021-02-12 11:17:00 +0100

    build(json lint): adding json lint checker

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const { src, dest, series } = require("gulp");
 const xo = require("gulp-xo");
 const stylelint = require("gulp-stylelint");
 const htmlhint = require("gulp-htmlhint");
+const jsonlint = require("gulp-jsonlint");
 const prettier = require("gulp-prettier");
 
 /*
@@ -31,7 +32,7 @@ function html() {
 /*
 validate javascript with eslint, ignores assetes/plugins
 */
-function eslintXO() {
+function jsLintXO() {
   return src(["assets/js/**/*.js"])
     .pipe(
       xo({
@@ -42,17 +43,28 @@ function eslintXO() {
     .pipe(xo.failAfterError()); // fail on errors
 }
 
+/*
+validate json files
+*/
+function jsonLint() {
+  return src(["package.json", ".stylelintrc.json", ".xo-config.json"])
+    .pipe(jsonlint())
+    .pipe(jsonlint.failOnError());
+}
+
 // can be triggered locally to format your code
 exports.prettier = function () {
   return src([
     "assets/js/*.js",
-    "assets/css/*.css",
     ".prettierrc.js",
     "gulpfile.js",
+    "assets/css/*.css",
     "*.html",
+    ".github/**/*.yml",
+    "package.json",
   ])
     .pipe(prettier())
     .pipe(dest((file) => file.base));
 };
 // automatically triggered upon each push or pull request
-exports.validate = series(eslintXO, css, html);
+exports.validate = series(jsonLint, jsLintXO, css, html);
