406be36a675683b6c6c3e367e43c3206aad888d6
Rashad Jamara <rjamara@hexonet.net>
2021-02-18 14:26:25 +0100

    build(md styling): using prettier to style md files

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 const { src, series, dest } = require("gulp");
+const prettier = require("gulp-prettier");
 const through2 = require("through2");
 const MDLint = require("markdownlint");
+
+const fix_files = ["**/*.md", "!HISTORY.md", "!node_modules/**/*.md", "!dist/**/*.md"];
+// use prettier to format only
+function formatWithPrettier() {
+  return src(fix_files)
+    .pipe(prettier())
+    .pipe(dest((file) => file.base));
+}
 
 // lint markdown files
 function markDownLinter() {
   let errorFound = false;
-  return src(
-    ["**/*.md", "!HISTORY.md", "!node_modules/**/*.md", "!dist/**/*.md"],
-    { read: false }
-  )
+  return src(fix_files)
     .pipe(
       through2.obj(function (file, enc, next) {
         MDLint(
