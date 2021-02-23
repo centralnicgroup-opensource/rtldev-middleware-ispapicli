0e9d6e51af7c59d08eea770842e3fc394b64eceb
Rashad Jamara <rjamara@hexonet.net>
2021-02-23 12:48:30 +0100

    fix(fixed shellcode): ignoring non existing folders

--- scripts/win-pyinstaller.ps1
+++ scripts/win-pyinstaller.ps1
@@ -6,8 +6,8 @@ pyinstaller --onefile --windowed .\ispapicli.py
 cd .\dist
 New-Item -Path .\bin -ItemType Directory
 Move-Item -Path .\ispapicli.exe -Destination .\bin -PassThru
-Copy-Item -Path ..\commands -Destination .\ -Recurse -Force -Passthru
-Copy-Item -Path ..\config -Destination .\ -Recurse -Force -Passthru
-Copy-Item -Path ..\icons -Destination .\ -Recurse -Force -Passthru
+Copy-Item -Path ..\commands -Destination .\ -Recurse -Force -PassThru -ErrorAction SilentlyContinue
+Copy-Item -Path ..\config -Destination .\ -Recurse -Force -PassThru -ErrorAction SilentlyContinue
+Copy-Item -Path ..\icons -Destination .\ -Recurse -Force -PassThru
 cd ..
 Compress-Archive -Path .\dist -DestinationPath ..\..\win-binary-latest.zip
