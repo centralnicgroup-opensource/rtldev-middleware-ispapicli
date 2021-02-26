25dca8aa7779e7210402ce21439803b9f573c9e5
Rashad Jamara <rjamara@hexonet.net>
2021-02-26 09:49:24 +0100

    fix(pyinstaller): pyinstaller have different syntax on windows

--- scripts/win-pyinstaller.ps1
+++ scripts/win-pyinstaller.ps1
@@ -2,5 +2,5 @@
 echo "Executing Pyinstaller Windows..."
 pip install pyinstaller
 cd .\hexonet\ispapicli\
-pyinstaller --onefile ispapicli.py --add-data 'icons:data/icons'
+pyinstaller --onefile ispapicli.py --add-data 'icons;data/icons'
 Compress-Archive -Path .\dist -DestinationPath ..\..\win-binary-latest.zip
