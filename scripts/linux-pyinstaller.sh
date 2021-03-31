aae09ea0339f52ef1b48bfc31993a210dcec6a93
Rashad Jamara <rjamara@hexonet.net>
2021-03-31 11:18:53 +0200

    fix(binary): fixed bin generation script after adding update script

--- scripts/linux-pyinstaller.sh
+++ scripts/linux-pyinstaller.sh
@@ -4,6 +4,8 @@ pip install pyinstaller
 cd hexonet/ispapicli || exit
 pyinstaller --onefile ispapicli.py --add-data 'icons:data/icons'
 sudo chmod +x dist/ispapicli
-cp -r ../../scripts/linux-download.sh dist/scripts
+mkdir dist/scripts
+cp  ../../scripts/linux-download.sh dist/scripts
+cp  ../../scripts/win-download.ps1 dist/scripts
 zip -r linux-binary-latest.zip dist
 mv linux-binary-latest.zip ../../
--- scripts/win-pyinstaller.ps1
+++ scripts/win-pyinstaller.ps1
@@ -2,4 +2,6 @@ echo "Executing Pyinstaller Windows..."
 pip install pyinstaller
 cd .\hexonet\ispapicli\
 pyinstaller --onefile ispapicli.py --add-data 'icons;data/icons'
+mkdir dist\scripts
+move ..\..\scripts\linux-download.sh, ..\..\scripts\win-download.ps1 .\dist\scripts\
 Compress-Archive -Path .\dist -DestinationPath ..\..\win-binary-latest.zip
