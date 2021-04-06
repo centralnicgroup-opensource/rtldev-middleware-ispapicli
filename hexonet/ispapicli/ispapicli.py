924f3df17cc89fae14d0cd0c2e516206b4e80b0e
Rashad Jamara <rjamara@hexonet.net>
2021-04-06 11:45:40 +0200

    build(testpr): testing new settings

--- hexonet/ispapicli/ispapicli.py
+++ hexonet/ispapicli/ispapicli.py
@@ -26,7 +26,7 @@ from PyQt5.QtWidgets import QApplication
 
 
 def main(args):
-
+    # test gh-actions with env
     # create core logic object
     core_obj = Core()
     # get the python standard parser initialised
