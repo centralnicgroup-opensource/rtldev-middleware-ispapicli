9f86119e3ade49f09c9ead3148bf2f4509a77abe
Rashad Jamara <rjamara@hexonet.net>
2021-04-06 12:37:03 +0200

    perf(ghactions): testing env ghactions

--- hexonet/ispapicli/ispapicli.py
+++ hexonet/ispapicli/ispapicli.py
@@ -26,7 +26,7 @@ from PyQt5.QtWidgets import QApplication
 
 
 def main(args):
-
+    # test pull request env
     # create core logic object
     core_obj = Core()
     # get the python standard parser initialised
