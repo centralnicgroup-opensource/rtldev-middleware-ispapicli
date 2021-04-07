c529a433bfc7e7b5869ce576bb01ea425a9c3a20
Rashad Jamara <rjamara@hexonet.net>
2021-04-07 08:13:47 +0200

    perf(test): testing

--- hexonet/ispapicli/ispapicli.py
+++ hexonet/ispapicli/ispapicli.py
@@ -26,7 +26,6 @@ from PyQt5.QtWidgets import QApplication
 
 
 def main(args):
-    # comment added for testing
     # create core logic object
     core_obj = Core()
     # get the python standard parser initialised
