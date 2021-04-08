f23343253e02f9af2c154edae3840a786fad0632
Rashad Jamara <rjamara@hexonet.net>
2021-04-08 12:39:10 +0200

    fix(bin fix): fixed windows bin generation

--- scripts/win-pyinstaller.ps1
+++ scripts/win-pyinstaller.ps1
@@ -2,4 +2,4 @@ echo "Executing Pyinstaller Windows..."
 pip install pyinstaller
 cd .\hexonet\ispapicli\
 pyinstaller --onefile ispapicli.py --add-data 'icons;data/icons'
-Compress-Archive -Path .\dist\ispapicli -DestinationPath ..\..\win-binary-latest.zip
+Compress-Archive -Path .\dist\ispapicli.exe -DestinationPath ..\..\win-binary-latest.zip
