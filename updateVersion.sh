339f4c90c2ec3b41675b828e806f613c015c5cc1
Kai Schwarz <kschwarz@hexonet.net>
2021-04-08 10:47:52 +0200

    ci(update version): add binary renaming actions

--- updateVersion.sh
+++ updateVersion.sh
@@ -10,3 +10,7 @@ sed -i "s/[0-9]\+\.[0-9]\+\.[0-9]\+/${newversion}/g" VERSION
 sed -i "s/__version__ = \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/__version__ = \"${newversion}\"/g" hexonet/ispapicli/__init__.py
 sed -i "s/__version__ = \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/__version__ = \"${newversion}\"/g" hexonet/ispapicli/modules/core.py
 sed -i "s/__version__ = \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/__version__ = \"${newversion}\"/g" hexonet/ispapicli/gui/mainframe.py
+
+mv artifacts/macos-binary-latest.zip mv "artifacts/macos-binary-${newversion}.zip"
+mv artifacts/linux-binary-latest.zip mv "artifacts/linux-binary-${newversion}.zip"
+mv artifacts/win-binary-latest.zip mv "artifacts/win-binary-${newversion}.zip"
