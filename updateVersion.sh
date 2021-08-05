4399a81ebad6174e4ef3b1cec3814aff8abba49c
Rashad Jamara <rjamara@hexonet.net>
2021-08-05 16:02:05 +0200

    fix(testci): testing sematic release version change

--- updateVersion.sh
+++ updateVersion.sh
@@ -11,6 +11,6 @@ sed -i "s/__version__ = \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/__version__ = \"${newversi
 sed -i "s/__version__ = \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/__version__ = \"${newversion}\"/g" hexonet/ispapicli/modules/core.py
 sed -i "s/__version__ = \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/__version__ = \"${newversion}\"/g" hexonet/ispapicli/gui/mainframe.py
 
-mv artifacts/macos-binary/macos-binary-latest.zip "artifacts/macos-binary/macos-binary-${newversion}.zip"
-mv artifacts/linux-binary/linux-binary-latest.zip "artifacts/linux-binary/linux-binary-${newversion}.zip"
-mv artifacts/win-binary/win-binary-latest.zip "artifacts/win-binary/win-binary-${newversion}.zip"
+# mv artifacts/macos-binary/macos-binary-latest.zip "artifacts/macos-binary/macos-binary-${newversion}.zip"
+# mv artifacts/linux-binary/linux-binary-latest.zip "artifacts/linux-binary/linux-binary-${newversion}.zip"
+# mv artifacts/win-binary/win-binary-latest.zip "artifacts/win-binary/win-binary-${newversion}.zip"
