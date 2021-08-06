f71f95eb99143562a4ddbe1cfb33b733ef90bc7d
Rashad Jamara <rjamara@hexonet.net>
2021-08-06 10:58:51 +0200

    feat(toolupdate): semantic release fix

--- updateVersion.sh
+++ updateVersion.sh
@@ -6,11 +6,12 @@
 
 # version format: X.Y.Z
 newversion="$1";
+echo "update version script started: version ${newversion}"
 sed -i "s/[0-9]\+\.[0-9]\+\.[0-9]\+/${newversion}/g" VERSION
 sed -i "s/__version__ = \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/__version__ = \"${newversion}\"/g" hexonet/ispapicli/__init__.py
 sed -i "s/__version__ = \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/__version__ = \"${newversion}\"/g" hexonet/ispapicli/modules/core.py
 sed -i "s/__version__ = \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/__version__ = \"${newversion}\"/g" hexonet/ispapicli/gui/mainframe.py
-
+echo "update version script finished"
 # mv artifacts/macos-binary/macos-binary-latest.zip "artifacts/macos-binary/macos-binary-${newversion}.zip"
 # mv artifacts/linux-binary/linux-binary-latest.zip "artifacts/linux-binary/linux-binary-${newversion}.zip"
 # mv artifacts/win-binary/win-binary-latest.zip "artifacts/win-binary/win-binary-${newversion}.zip"
